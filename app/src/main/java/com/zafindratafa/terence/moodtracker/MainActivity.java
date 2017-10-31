0d46f842babe068ad474808602dc40ea31c3fd13
Tez <t.zafindratafa@gmail.com>
2017-10-31 17:15:09 +0100

    vertical ViewPager implemented

--- app/src/main/java/com/zafindratafa/terence/moodtracker/MainActivity.java
+++ app/src/main/java/com/zafindratafa/terence/moodtracker/MainActivity.java
@@ -5,14 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    ViewPager mViewPager;
+    VerticalViewPager mViewPager;
     CustomSwipeAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mViewPager = (ViewPager)findViewById(R.id.view_pager);
+        mViewPager = (VerticalViewPager)findViewById(R.id.view_pager);
         mAdapter = new CustomSwipeAdapter(this);
         mViewPager.setAdapter(mAdapter);
     }
