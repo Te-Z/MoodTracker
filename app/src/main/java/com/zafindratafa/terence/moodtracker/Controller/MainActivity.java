2db07d6be4e32e917ac1597ef52206a6c2909ba6
Tez <t.zafindratafa@gmail.com>
2017-11-01 16:35:38 +0100

    HistoryActivity.java added and  linked to MainActivity.java

--- app/src/main/java/com/zafindratafa/terence/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/zafindratafa/terence/moodtracker/Controller/MainActivity.java
@@ -1,8 +1,11 @@
 package com.zafindratafa.terence.moodtracker.Controller;
 
+import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.zafindratafa.terence.moodtracker.R;
 import com.zafindratafa.terence.moodtracker.View.CustomSwipeAdapter;
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     private VerticalViewPager mViewPager;
     private CustomSwipeAdapter mAdapter;
     private int mCurrentItem = 2;
+    private ImageButton mAddNote, mHistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageScrollStateChanged(int state) {
 
+            }
+        });
+
+        mAddNote = (ImageButton)findViewById(R.id.add_note);
+        mHistory = (ImageButton)findViewById(R.id.history_display);
+        mHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent historyActivity = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(historyActivity);
             }
         });
     }
