c389fcca64880a2d52cfcef09ac3cb5d9332d62e
Tez <t.zafindratafa@gmail.com>
2017-11-01 11:10:54 +0100

    background colors added

--- app/src/main/java/com/zafindratafa/terence/moodtracker/CustomSwipeAdapter.java
+++ app/src/main/java/com/zafindratafa/terence/moodtracker/CustomSwipeAdapter.java
@@ -1,7 +1,9 @@
 package com.zafindratafa.terence.moodtracker;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +17,7 @@ import android.widget.TextView;
 
 public class CustomSwipeAdapter extends PagerAdapter {
     private int[] image_resources = {R.drawable.sad, R.drawable.disappointed, R.drawable.normal, R.drawable.happy, R.drawable.superhappy};
+    private String[] background_color = {"#ffde3c50", "#ff9b9b9b", "#a5468ad9", "#ffb8e986", "#fff9ec4f"};
     private Context ctx;
     private LayoutInflater mLayoutInflater;
 
@@ -36,10 +39,9 @@ public class CustomSwipeAdapter extends PagerAdapter {
     public Object instantiateItem(ViewGroup container, int position) {
         mLayoutInflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View item_view = mLayoutInflater.inflate(R.layout.swipe_layout, container, false);
+        item_view.setBackgroundColor(Color.parseColor(background_color[position]));
         ImageView imageView = (ImageView) item_view.findViewById(R.id.image_view);
-        TextView textView = (TextView) item_view.findViewById(R.id.image_count);
         imageView.setImageResource(image_resources[position]);
-        textView.setText("Image : "+position);
         container.addView(item_view);
         return item_view;
     }
