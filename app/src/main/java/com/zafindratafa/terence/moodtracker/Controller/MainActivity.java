57c40b593ed49512d5b120bf23aab81fb34901d8
Tez <t.zafindratafa@gmail.com>
2017-11-02 08:12:32 +0100

    Last position saving added

--- app/src/main/java/com/zafindratafa/terence/moodtracker/Controller/MainActivity.java
+++ app/src/main/java/com/zafindratafa/terence/moodtracker/Controller/MainActivity.java
@@ -1,11 +1,13 @@
 package com.zafindratafa.terence.moodtracker.Controller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.zafindratafa.terence.moodtracker.R;
 import com.zafindratafa.terence.moodtracker.View.CustomSwipeAdapter;
@@ -14,8 +16,12 @@ import com.zafindratafa.terence.moodtracker.View.VerticalViewPager;
 public class MainActivity extends AppCompatActivity {
     private VerticalViewPager mViewPager;
     private CustomSwipeAdapter mAdapter;
-    private int mCurrentItem = 2;
+    private int mCurrentMood;
     private ImageButton mAddNote, mHistory;
+    private TextView mTextView;
+
+    public static final String MOOD_OF_THE_DAY = "mood_of_the_day";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,16 @@ public class MainActivity extends AppCompatActivity {
         mViewPager = (VerticalViewPager)findViewById(R.id.view_pager);
         mAdapter = new CustomSwipeAdapter(this);
         mViewPager.setAdapter(mAdapter);
-        mViewPager.setCurrentItem(mCurrentItem);
+        // load the last position of the day
+        //TODO: check if the currentPosition was setted up today
+        //TODO: if the currentPosition is yesterday's, save it in the log file with his comment and the date
+        mCurrentMood = getPreferences(MODE_PRIVATE).getInt(MOOD_OF_THE_DAY, 2);
+
+        mTextView = (TextView)findViewById(R.id.textview_test);
+        mTextView.setText("Last selected view: "+mCurrentMood);
+
+        mViewPager.setCurrentItem(mCurrentMood);
+        //TODO: play a sound each time the view change
         mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -51,5 +66,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(historyActivity);
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        // Save currentMood when exiting the app
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        preferences.edit().putInt(MOOD_OF_THE_DAY, mViewPager.getCurrentItem()).apply();
+        super.onStop();
     }
 }
